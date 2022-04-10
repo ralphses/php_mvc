1019aeb122ca582c5d304237193d75296ea6601c
ralphses <eze.raph@gmail.com>
2022-04-10 01:02:38 +0100

    Implimented forms and validation

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ use src\controllers\AuthController;
 use src\controllers\SiteController;
 use src\core\Application;
 
-//Create a new instance of application
+//Create a new instance of application and pass the current directory to the constructor as the root path of the application
 $app = new Application(dirname(__DIR__));
 
-//Configure all the possible routes
+//Configure all the possible routes for your website
 
 $app->router->get('/', [SiteController::class, 'home']);
 
